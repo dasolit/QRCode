4d5dbc7140a141d06c58c6ef3875c312a2b9870d
itmdeveloper <choidasol95@gmail.com>
2024-12-18 21:45:28 +0900

    QR 데이터 map -> String 변경

--- src/module/QRCode.java
+++ src/module/QRCode.java
@@ -1,26 +1,20 @@
 package module;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 import java.util.List;
 
 public class QRCode {
     private String color;
-    private Map<Object, Object> data = new HashMap<>();
+    private String data;
     private List<QRCodeModule> modules = new ArrayList<>();
 
     public void setColor(String color) {
         this.color = color;
     }
 
-    public void setData(Map<Object, Object> data) {
+    public void setData(String data) {
         this.data = data;
-    }
-
-    public void addData(Object key, Object value) {
-        this.data.put(key, value);
     }
 
     public QRCode addModule(QRCodeModule module) {
