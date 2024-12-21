011d78468ace8d234cee43194277134559a0664a
itmdeveloper <choidasol95@gmail.com>
2024-12-21 13:30:06 +0900

    gradle 프로젝트 추가, 패키지 구조 변경, 테스트 코드 추가

---
+++ src/test/java/QrCodeTest.java
@@ -0,0 +1,72 @@
+
+import module.QrCode;
+import module.QrModule;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class QrCodeTest {
+
+  @Test
+  public void testSetData() {
+    QrCode qrCode = new QrCode("initial");
+    qrCode.setData("newData");
+    assertEquals("newData", qrCode.getData());
+  }
+
+  @Test
+  public void testSetColor() {
+    QrCode qrCode = new QrCode("test");
+    qrCode.setColor(0xFF0000); // Red color
+  }
+
+  @Test
+  public void testSetImage() {
+    QrCode qrCode = new QrCode("test");
+    qrCode.setImage("custom_image", "jpg");
+  }
+
+  @Test
+  public void testAddModule() {
+    QrCode qrCode = new QrCode("test");
+    QrModule mockModule = (qr) -> {};
+    qrCode.addModule(mockModule);
+  }
+
+  @Test
+  public void testInitialize() throws IOException {
+    QrCode qrCode = new QrCode("https://example.com");
+    qrCode.setImage("test_qr_code", "png");
+    qrCode.setColor(0x000000);
+    qrCode.initialize();
+
+    File file = new File("test_qr_code.png");
+    assertTrue(file.exists());
+    if (file.exists()) {
+      file.delete();
+    }
+  }
+
+  @Test
+  public void testEncodeText() {
+    QrCode qrCode = QrCode.encodeText("https://example.com", QrCode.Ecc.LOW);
+    assertNotNull(qrCode);
+  }
+
+  @Test
+  public void testEncodeBinary() {
+    QrCode qrCode = QrCode.encodeBinary("binaryData".getBytes(), QrCode.Ecc.MEDIUM);
+    assertNotNull(qrCode);
+  }
+
+  @Test
+  public void testVersionAndSize() {
+    QrCode qrCode = QrCode.encodeText("VersionTest", QrCode.Ecc.LOW);
+    assertTrue(qrCode.version >= QrCode.MIN_VERSION && qrCode.version <= QrCode.MAX_VERSION);
+    assertTrue(qrCode.size > 0);
+  }
+
+}
